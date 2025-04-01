80234b13501ed527f737512d04fd66608c226ebf
Descalzo <46046116+Descalzo@users.noreply.github.com>
2025-04-01 19:01:49 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2028,10 +2028,10 @@ async function iniciarPollingPago() {
 
       if (transaccionConfirmada) {
         const restaurante = users.find(u => u.email === transaccionConfirmada.to);
+	mostrarConfirmacionPago(transaccionConfirmada.amount); 
+        mostrarAnimacionExito();
         qrContainer.innerHTML = `
           <div style="text-align: center; color: green; padding: 20px;">
-	     mostrarConfirmacionPago(transaccionConfirmada.amount); 
-             mostrarAnimacionExito();
             <h3>✅ Pago confirmado</h3>
             <p>Has pagado ${formatearEuros(transaccionConfirmada.amount)} a ${'Restaurante'}</p>
             <p>Fecha: ${formatearFecha(transaccionConfirmada.date)}</p>
